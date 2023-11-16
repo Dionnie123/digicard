fdff716211f9812405f9b9ec4b960b0de50cf82c
Mark Dionnie Bulingit <dionnie_bulingit@yahoo.com>
2023-11-16 20:06:55 +0800

    up

--- lib/app/app.locator.dart
+++ lib/app/app.locator.dart
@@ -38,7 +38,7 @@ Future<void> setupLocator({
   locator.registerLazySingleton(() => RouterService());
   locator.registerLazySingleton(() => UserService());
   final supabaseAuthService = SupabaseAuthService();
-  await supabaseAuthService.initialise();
+  await supabaseAuthService.init();
   locator.registerSingleton<AuthService>(supabaseAuthService);
 
   locator.registerLazySingleton(() => BottomSheetService());
